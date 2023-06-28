1e0988c726b32652b66e1391e69f2fe6d61f6c7f
vetalmolotov <vetal.molotov@gmail.com>
2023-06-28 19:16:00 +0300

    add basic slider functional

--- src/index.js
+++ src/index.js
@@ -3,14 +3,58 @@
 //
 // // Test import of an asset
 // import webpackLogo from '@/images/webpack-logo.svg'
+// import { test } from 'http://www.mocky.io/v2/5dfcef48310000ee0ed2c281'
 
 // Test import of styles
 import '@/styles/index.scss'
+
+
 
 const showLoginButton = document.querySelector('#showLogin')
 const loginForm = document.querySelector('#loginForm')
 showLoginButton.addEventListener('click', () => {
   loginForm.classList.add('open');
+})
+
+const slider = document.querySelector('#slider')
+const next = document.querySelector('#next')
+const slide1 = document.querySelector('#slide1')
+const slide2 = document.querySelector('#slide2')
+const slide3 = document.querySelector('#slide3')
+const slide4 = document.querySelector('#slide4')
+const slide5 = document.querySelector('#slide5')
+let color = true
+next.addEventListener('click', () => {
+  // if (slider.classList.contains('slide1')) {
+  if (color) {
+    slide1.classList.remove('show')
+    slide1.classList.add('hide')
+    slide2.classList.add('show')
+    slider.classList.remove('slide1')
+    slider.classList.add('slide2')
+    next.classList.add('next3')
+  }
+
+  // if (next.classList.contains('next3')) {
+  if (!color) {
+    if ( slider.classList.contains('slide2') ) {
+      slide2.classList.remove('show')
+      slide2.classList.add('hide')
+      slide3.classList.add('show')
+      slider.classList.remove('slide2')
+      slider.classList.add('slide3')
+    }
+  }
+  if (color) {
+    if ( slider.classList.contains('slide3') ) {
+      slide3.classList.remove('show')
+      slide3.classList.add('hide')
+      slide4.classList.add('show')
+      slider.classList.remove('slide3')
+      slider.classList.add('slide4')
+    }
+  }
+  color = !color;
 })
 
 // Appending to the DOM
